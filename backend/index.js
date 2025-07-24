e7e42aa304e8a1f7639625176f6be038f28e2bc4
Mohamed Mufeez <129639317+IT22320896@users.noreply.github.com>
2025-07-24 22:03:56 +0530

    created index.js

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import examRoutes from "./routes/examRoutes.js";
+import resultRoutes from "./routes/resultRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/exams", examRoutes);
+app.use("/api/results", resultRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+  })
+  .catch((err) => console.log(err));
